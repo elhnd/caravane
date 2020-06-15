c3f89a9822f902a42f82860004271b249659cb03
elhnd <eldji22@hotmail.fr>
2020-06-15 19:41:14 +0000

    cache clear

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -60,7 +60,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="text")
      * @Groups({"users_read","user_write"})
      */
     private $roles = [];
